6c1dd3376d4900c94b978139749c3f2ff34c7031
nkosinathintuli <nkosinathintuli21@gmail.com>
2021-10-05 12:05:19 +0100

    added variables

--- ADC.py
+++ ADC.py
@@ -4,6 +4,12 @@ import board
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 
+# button
+btn = 16
+
+# sampling rates
+samp_rate = [1, 5, 10]
+
 # create the spi bus
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
@@ -14,8 +20,11 @@ cs = digitalio.DigitalInOut(board.D5)
 # create the mcp object
 mcp = MCP.MCP3008(spi, cs)
 
-# create an analog input channel on pin 0
-chan = AnalogIn(mcp, MCP.P0)
+# create an analog input channel on pin 1,2 (CH1,CH2)
+chan1 = AnalogIn(mcp, MCP.P1) # Temp
+chan2 = AnalogIn(mcp, MCP.P2) # LDR
 
-print("Raw ADC Value: ", chan.value)
-print("ADC Voltage: " + str(chan.voltage) + "V")
+
+
+print("Raw ADC Value: ", chan1.value)
+print("ADC Voltage: " + str(chan1.voltage) + "V")
